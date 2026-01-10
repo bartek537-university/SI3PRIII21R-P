f5b0e44f64f8ccb622fc65ad13d7c5ceeed0723a
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-01-10 16:35:08 +0100

    Create project

---
+++ src/main/java/pl/bartek537/snapdrop/SnapdropApplication.java
@@ -0,0 +1,13 @@
+package pl.bartek537.snapdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapdropApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SnapdropApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bartek537/snapdrop/SnapdropApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartek537.snapdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapdropApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
