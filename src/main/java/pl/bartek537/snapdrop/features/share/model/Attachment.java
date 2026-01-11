9236f10893baa99e89fefed4953e7d77405c03a7
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2026-01-11 14:58:36 +0100

    Create one-to-many relationship between Share and Attachment

--- src/main/java/pl/bartek537/snapdrop/features/share/model/Attachment.java
+++ src/main/java/pl/bartek537/snapdrop/features/share/model/Attachment.java
@@ -1,19 +1,23 @@
 package pl.bartek537.snapdrop.features.share.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
 @Entity
+@Table
 public class Attachment {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String fileName;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    @JsonIgnoreProperties("attachments")
+    private Share share;
 
     public UUID getId() {
         return this.id;
@@ -25,5 +29,13 @@ public class Attachment {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public Share getShare() {
+        return this.share;
+    }
+
+    public void setShare(Share share) {
+        this.share = share;
     }
 }
--- src/main/java/pl/bartek537/snapdrop/features/share/model/Share.java
+++ src/main/java/pl/bartek537/snapdrop/features/share/model/Share.java
@@ -1,19 +1,32 @@
 package pl.bartek537.snapdrop.features.share.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
+@Table
 public class Share {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @OneToMany(mappedBy = "share", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("share")
+    private Set<Attachment> attachments;
+
     public UUID getId() {
         return this.id;
+    }
+
+    public Set<Attachment> getAttachments() {
+        return this.attachments;
+    }
+
+    public void addAttachment(Attachment attachment) {
+        this.attachments.add(attachment);
+        attachment.setShare(this);
     }
 }
